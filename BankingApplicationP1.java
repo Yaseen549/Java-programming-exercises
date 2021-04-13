cfa31e85560c230b34d5b9dfb364502b29fe7ad3
Yaseen <fantasticyaseenshariff@gmail.com>
2021-04-13 13:34:17 +0530

    BankingApplicationP1

---
+++ BankingApplicationP1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BankingApplicationP1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Banking Application!");
+        System.out.println("Our operations:");
+        System.out.println("Click 1 for exiting from system");
+        System.out.println("Click 2 for money transfer!");
+        System.out.println("Click 3 to get money!");
+
+        int operation = scanner.nextInt();
+
+        if(operation == 1){
+            System.out.println("Exiting from system... ");
+        }
+        else if(operation == 2){
+            System.out.println("You choose money transferring, you can transfer money");
+        }
+        else if(operation == 3){
+            System.out.println("you can get money...");
+        }
+        else{
+            System.out.println("Invalid Operation!");
+        }
+
+
+
+    }
+}

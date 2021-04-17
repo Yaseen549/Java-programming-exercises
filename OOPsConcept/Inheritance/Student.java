7fb11c4cd4b78905820955e97c02c587b7c71f57
Yaseen <fantasticyaseenshariff@gmail.com>
2021-04-17 19:27:48 +0530

    Inheritance

---
+++ OOPsConcept/Inheritance/Student.java
@@ -0,0 +1,13 @@
+public class Student extends Person{
+    // super class is a base class that we extends
+
+        public Student(String name, String surname, int age){
+
+            super(name, surname, age);
+
+        }
+
+        // Person is a parent class or super class or base class
+    // teacher or student is our child class
+
+}
---
+++ OOPsConcept/Inheritance/Teacher.java
@@ -0,0 +1,19 @@
+public class Teacher extends Person{
+    // super class is a base class that we extends
+
+    public Teacher(String name, String surname, int age){
+
+        super(name, surname, age);
+
+    }
+
+    // Person is a parent class or super class or base class
+    // teacher or student is our child class
+
+    // super() <-- means, go and call constructor of a super class
+    // super. <-- means, go and call variables of super class
+
+    // override
+    // go and call a method from a super class, and write something over it.
+
+}

c0214228509ea2f7c10d87a9de81d4068cd15a8a
Yaseen <fantasticyaseenshariff@gmail.com>
2021-04-17 14:00:26 +0530

    Arithmetic operations

---
+++ maths/Arithmetic/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        int add = Maths.addition(10,5);
+        int sub = Maths.subtraction(10,5);
+        int mul = Maths.multiplication(10,5);
+        int div = Maths.division(10,5);
+        int rem = Maths.reminder(10,5);
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(rem);
+    }
+}
---
+++ maths/Arithmetic/Maths.java
@@ -0,0 +1,17 @@
+public class Maths {
+    public static int addition(int a, int b) {
+        return a+b;
+    }
+    public static int subtraction(int a, int b) {
+        return a-b;
+    }
+    public static int multiplication(int a, int b) {
+        return a*b;
+    }
+    public static int division(int a, int b) {
+        return a/b;
+    }
+    public static int reminder(int a, int b) {
+        return a%b;
+    }
+}

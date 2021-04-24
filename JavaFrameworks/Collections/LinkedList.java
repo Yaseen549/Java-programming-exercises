a5f5d160d0b903900970e6cf274aeb4856e8e2d8
Yaseen <fantasticyaseenshariff@gmail.com>
2021-04-24 14:09:07 +0530

    LinkedList - Collection

---
+++ JavaFrameworks/Collections/LinkedList.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class LinkedList {
+    public static void main(String[] args) {
+
+        LinkedList<String> list1 = new LinkedList<>();
+
+        list1.add("Name");
+        list1.add("Name");
+        list1.add("Name2");
+
+        Iterator<String> itr=list1.iterator();
+
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+            itr.remove();
+            System.out.println("!");
+        }
+
+
+    }
+}

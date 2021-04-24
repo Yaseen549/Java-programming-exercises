ef163abd1564da8d605d4568f09a1016825af70e
Yaseen <fantasticyaseenshariff@gmail.com>
2021-04-24 14:24:31 +0530

    Collections - List

---
+++ JavaFrameworks/Collections/List/ArrayList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<String> list1 = new ArrayList<>();
+
+        list1.add("Name");
+        list1.add("Name");
+        list1.add("Name2");
+
+        Iterator itr=list1.iterator();
+
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+            itr.remove();
+            System.out.println(itr.next());
+            System.out.println("!");
+        }
+    }
+}
